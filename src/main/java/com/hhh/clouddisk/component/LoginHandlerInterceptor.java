5ead861fa5f667bb86162f6409bba5d52cc6fdaf
hhh777yyy <1119848245@qq.com>
2020-04-09 14:07:41 +0800

    v0.3 配置登陆拦截功能

---
+++ src/main/java/com/hhh/clouddisk/component/LoginHandlerInterceptor.java
@@ -0,0 +1,25 @@
+package com.hhh.clouddisk.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (loginUser==null){
+            //未登录,返回登录页面
+            response.sendRedirect("/");
+            return false;
+
+        }else {
+            //已登录,放行
+            return true;
+        }
+    }
+}
